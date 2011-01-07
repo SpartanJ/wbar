47d9f873bdeb103ea5df73feb6efe970abb23961
yadickson <yadickson@3883df14-be21-0410-8dd7-a90e1ff62243>
2011-01-07 20:25:26 +0000

    remove code old update depend wbar

--- src/config/Main.cc
+++ src/config/Main.cc
@@ -15,6 +15,7 @@ void signals(int sig)
 int main(int argc, char **argv)
 {
     #ifdef ENABLE_NLS
+    setlocale( LC_ALL, "" );
     bindtextdomain(GETTEXT_PACKAGE, GNOMELOCALEDIR);
     bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
     textdomain(GETTEXT_PACKAGE);
--- src/core/Main.cc
+++ src/core/Main.cc
@@ -19,6 +19,7 @@ void corpshandler(int);
 int main(int argc, char **argv)
 {
     #ifdef ENABLE_NLS
+    setlocale( LC_ALL, "" );
     bindtextdomain(GETTEXT_PACKAGE, GNOMELOCALEDIR);
     bind_textdomain_codeset(GETTEXT_PACKAGE, "UTF-8");
     textdomain(GETTEXT_PACKAGE);
